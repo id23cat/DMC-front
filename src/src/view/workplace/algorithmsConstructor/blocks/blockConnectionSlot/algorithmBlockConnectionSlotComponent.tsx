6acac0a240ef925f48d0586a95c305d6e551e8ff
KyMback <Seregick@hotmail.com>
2020-03-10 22:37:37 +0300

    Added higliting while connect

--- src/src/view/workplace/algorithmsConstructor/blocks/blockConnectionSlot/algorithmBlockConnectionSlotComponent.tsx
+++ src/src/view/workplace/algorithmsConstructor/blocks/blockConnectionSlot/algorithmBlockConnectionSlotComponent.tsx
@@ -4,20 +4,24 @@ import { AlgorithmBlockConnectionSlotStore, ConnectionType } from "./algorithmBl
 import { Circle, Group } from "react-konva";
 import { Text as KonvaText } from "react-konva/lib/ReactKonvaCore";
 import React from "react";
+import { useAlgorithmsBlocksConnectionContext } from "../../hooks";
 
 const connectionSlotRadius = 7;
 
 export const AlgorithmBlockConnectionSlot = observer(({ store }: PropsWithStore<AlgorithmBlockConnectionSlotStore>) => {
+    const connectionContext = useAlgorithmsBlocksConnectionContext();
+    const isSelectedForConnection = connectionContext.to === store || connectionContext.from === store;
+
     return (
         <Group x={store.x} y={store.y}>
-            <Circle radius={connectionSlotRadius} fill="black" />
+            <Circle radius={connectionSlotRadius} fill={isSelectedForConnection ? "red" : "black"} />
             <KonvaText
                 y={-15}
                 x={store.type === ConnectionType.In ? -80 : 20}
                 align={ConnectionType.In ? "right" : "right"}
                 fontSize={12}
                 text={store.contract}
-                fill="black"
+                fill={isSelectedForConnection ? "red" : "black"}
             />
         </Group>
     );
